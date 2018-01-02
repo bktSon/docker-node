c3df337c56302fd63e058c8dca9a49c687859e12
achiliku <kimsonbui303@gmail.com>
2018-01-02 21:38:36 +0700

    second commit

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res){
   res.send('Hello world from Distelli & Docker!');
 });
 
+app.get('/run', function (req, res) {
+   res.send('hello world')
+});
+
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
 
