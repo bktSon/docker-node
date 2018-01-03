cacd588d544a77d647906cdcb8c68b625d121725
achiliku <kimsonbui303@gmail.com>
2018-01-03 21:14:02 +0700

    add jasmin

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var exports = module.export = {};
+var exports = module.exports = {};
 
 app.get('/', function(req, res){
     res.send('Hello World');
